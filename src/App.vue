37454b776f043c4baf86e067d2788a93cd47c546
nza <1484663282@qq.com>
2019-09-15 15:31:45 +0800

    项目目录初始化

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
+<!-- 单文件组件 -->
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 当前路由地址所对应的内容 -->
     <router-view/>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+// 使用fastClick 解决移动端点击事件会延迟 300ms 执行的问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
