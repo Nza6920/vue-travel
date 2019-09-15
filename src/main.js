b55e1ae314ad877a11268493a3c5fa95ddb78120
nza <1484663282@qq.com>
2019-09-15 16:42:58 +0800

    Finish header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
 import fastClick from 'fastclick'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 // 使用fastClick 解决移动端点击事件会延迟 300ms 执行的问题
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    Hello World
+      <home-header></home-header>
   </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {HomeHeader},
+  comments: {
+    // 全写 HomeHeader: HomeHeader
+    HomeHeader
+  }
 }
 </script>
 <style>
